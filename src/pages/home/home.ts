264398fd988ae7d1a1c39992e9f1175a61ac7d48
sakucodes <_saku@live.de>
2017-05-28 23:51:10 +0200

    add video, screen orientation and fullscreen-ish behavior

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ElementRef, ViewChild, Renderer } from '@angular/core';
+import { Content, NavController, Platform, DomController } from 'ionic-angular';
+import { ScreenOrientation } from "@ionic-native/screen-orientation";
+import { StatusBar } from "@ionic-native/status-bar";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,78 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('video', { read: ElementRef }) video: ElementRef;
+  @ViewChild('controls', { read: ElementRef }) videoControls: ElementRef;
 
+  isFullScreen: boolean = false;
+  areControlsVisible: boolean = true;
+  isLoading: boolean = false;
+  time: number = 0;
+  timeString: string = "00:00";
+
+  private controlsVisibleTimer: number = null;
+
+  constructor(public navCtrl: NavController,
+    private screenOrientation: ScreenOrientation,
+    private renderer: Renderer,
+    private domController: DomController,
+    private platform: Platform,
+    private statusBar: StatusBar) {
+    this.screenOrientation.onChange().subscribe(() => {
+      if (this.screenOrientation.type === this.screenOrientation.ORIENTATIONS.LANDSCAPE_PRIMARY || this.screenOrientation.type === this.screenOrientation.ORIENTATIONS.LANDSCAPE_SECONDARY) {
+        this.statusBar.hide();
+        this.isFullScreen = true;
+        this.domController.write(() => {
+          if (this.video) {
+            this.renderer.setElementStyle(this.video.nativeElement, "height", this.platform.width() + "px");
+          }
+        });
+      }
+      else {
+        this.isFullScreen = false;
+        this.statusBar.show();
+        this.domController.write(() => {
+          this.renderer.setElementStyle(this.video.nativeElement, "height", "auto");
+        });
+      }
+    });
   }
 
+  private updateTime(): void {
+    this.isLoading = false;
+    const video = this.video.nativeElement as HTMLVideoElement;
+    this.time = video.currentTime;
+    const h = Math.floor(this.time / 60);
+    const s = Math.floor(this.time % 60);
+    this.timeString = `${('0' + h).slice(-2)}:${('0' + s).slice(-2)}`;
+  }
+
+  timeSelected(): void {
+    this.updateTime();
+  }
+
+  togglePlayState(): void {
+    const video = this.video.nativeElement as HTMLVideoElement;
+
+    if (video.paused) {
+      video.play();
+      this.areControlsVisible = false;
+    }
+    else {
+      video.pause();
+      this.areControlsVisible = true;
+    }
+  }
+
+  showControls(): void {
+    this.areControlsVisible = true;
+    clearTimeout(this.controlsVisibleTimer);
+    if (!this.video.nativeElement.paused) {
+      this.controlsVisibleTimer = setTimeout(() => this.areControlsVisible = false, 1000);
+    }
+  }
+
+  loading(): void {
+    this.isLoading = true;
+  }
 }
